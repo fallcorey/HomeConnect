34a3448ccbeb54220dd211cd09bedfbc735d6fcd
fallcorey <fallcorey.a@gmail.com>
2025-09-26 20:38:31 +0200

    Delete app/src/main/java/com/secure/p2p/chat directory

--- app/src/main/java/com/secure/p2p/chat/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.secure.p2p.chat.utils
-
-object Constants {
-    const val MAX_CHAT_PARTICIPANTS = 8
-    const val MESSAGE_EPHEMERAL_TIMEOUT = 24 * 60 * 60 * 1000L // 24 hours
-    const val AES_KEY_SIZE = 256
-}
